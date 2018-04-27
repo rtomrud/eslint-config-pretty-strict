4d4df8fec504db4b79c042045e76c08c8ab0b789
Artem Rudenko <rtomrud@gmail.com>
2018-04-27 21:05:26 +0200

    Drop support for Node 4 and add support for Node 10

--- index.test.js
+++ index.test.js
@@ -1,37 +1,41 @@
-/* eslint-disable prefer-destructuring */
-
 "use strict";
 
 const test = require("tape");
 const eslint = require("eslint");
 const index = require("./index.js");
 
-test("eslint-config-pretty-strict with eslint", t => {
-  t.doesNotThrow(
+test("eslint-config-pretty-strict with eslint", ({ doesNotThrow, end }) => {
+  doesNotThrow(
     () => new eslint.CLIEngine({ configFile: "./index.js" }),
     "parsable and validated by ESLint's CLIEngine"
   );
-  t.end();
+  end();
 });
 
-test("eslint-config-pretty-strict with eslint-plugin-prettier", t => {
-  t.equal(
+test("eslint-config-pretty-strict with eslint-plugin-prettier", ({
+  equal,
+  end
+}) => {
+  equal(
     Array.isArray(index.plugins) && index.plugins.indexOf("prettier") !== -1,
     true,
     "uses eslint-plugin-prettier"
   );
-  t.equal(
+  equal(
     index.rules["prettier/prettier"][0],
     "error",
     "turns on prettier rule as an error"
   );
-  t.end();
+  end();
 });
 
-test("eslint-config-pretty-strict max-len with eslint-plugin-prettier", t => {
-  const maxLen = index.rules["max-len"][1];
-  const prettier = index.rules["prettier/prettier"][1];
-  t.equal(maxLen.code, prettier.printWidth, "uses Prettier's printWidth");
-  t.equal(maxLen.tabWidth, prettier.tabWidth, "uses Prettier's tabWidth");
-  t.end();
+test("eslint-config-pretty-strict max-len with eslint-plugin-prettier", ({
+  equal,
+  end
+}) => {
+  const [, maxLen] = index.rules["max-len"];
+  const [, prettier] = index.rules["prettier/prettier"];
+  equal(maxLen.code, prettier.printWidth, "uses Prettier's printWidth");
+  equal(maxLen.tabWidth, prettier.tabWidth, "uses Prettier's tabWidth");
+  end();
 });
