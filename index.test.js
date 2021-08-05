252fda876c74d2b1c1bd0362bad88eb2918cf079
Artem Rudenko <rtomrud@gmail.com>
2021-08-05 16:33:51 +0200

    Add jest dev dependency
    And drop tape.

--- index.test.js
+++ index.test.js
@@ -1,40 +1,23 @@
 "use strict";
 
-const test = require("tape");
 const { ESLint } = require("eslint");
 const { plugins, rules } = require("./index.js");
 
-test("eslint-config-pretty-strict with eslint", ({ doesNotThrow, end }) => {
-  doesNotThrow(
-    () => new ESLint({ overrideConfigFile: "./index.js" }),
-    "is parsed and validated by eslint"
-  );
-  end();
+test("eslint-config-pretty-strict with eslint", () => {
+  expect(() => new ESLint({ overrideConfigFile: "./index.js" })).not.toThrow();
 });
 
-test("eslint-config-pretty-strict with eslint-plugin-prettier", ({
-  equal,
-  end,
-}) => {
-  equal(
-    Array.isArray(plugins) && plugins.includes("prettier"),
-    true,
-    "enables eslint-plugin-prettier"
-  );
-  equal(
-    rules["prettier/prettier"],
-    "error",
-    "enables the prettier/prettier rule as an error"
-  );
-  equal(
-    rules["max-len"][0] === "error" && rules["max-len"][1].code,
-    80,
-    "avoids conflicts with the default value of prettier's --print-width option"
-  );
-  equal(
-    rules["no-tabs"][0] === "error" && rules["no-tabs"][1].allowIndentationTabs,
-    true,
-    "avoids conflicts with prettier's --use-tabs option"
-  );
-  end();
+test("eslint-config-pretty-strict with eslint-plugin-prettier", () => {
+  expect(plugins).toEqual(["prettier"]);
+  expect(rules["prettier/prettier"]).toBe("error");
+});
+
+test("eslint-config-pretty-strict with prettier's --print-width option", () => {
+  expect(rules["max-len"][0]).toBe("error");
+  expect(rules["max-len"][1].code).toBe(80);
+});
+
+test("eslint-config-pretty-strict with prettier's --use-tabs option", () => {
+  expect(rules["no-tabs"][0]).toBe("error");
+  expect(rules["no-tabs"][1].allowIndentationTabs).toBe(true);
 });
