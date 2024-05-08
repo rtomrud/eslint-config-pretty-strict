d2b3def9041e681e4d7511390da76cc1fcac7a9a
Artem Rudenko <rtomrud@gmail.com>
2024-05-08 20:14:06 +0200

    Drop deprecated rules

--- index.test.js
+++ index.test.js
@@ -6,15 +6,3 @@ import prettyStrict from "./index.js";
 test("eslint-config-pretty-strict with eslint", () => {
   assert.doesNotThrow(() => new ESLint({ overrideConfig: prettyStrict[0] }));
 });
-
-test("eslint-config-pretty-strict with prettier's --print-width option", () => {
-  const [{ rules }] = prettyStrict;
-  assert.equal(rules["max-len"][0], "error");
-  assert.equal(rules["max-len"][1].code, 80);
-});
-
-test("eslint-config-pretty-strict with prettier's --use-tabs option", () => {
-  const [{ rules }] = prettyStrict;
-  assert.equal(rules["no-tabs"][0], "error");
-  assert.equal(rules["no-tabs"][1].allowIndentationTabs, true);
-});
